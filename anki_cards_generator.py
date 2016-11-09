219d35c0dc5e94c39a9d4732614c413588156dc1
David Martínez <damaru2@users.noreply.github.com>
2016-11-09 19:13:52 +0000

    fix to allow definitions that have no example

--- anki_cards_generator.py
+++ anki_cards_generator.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import os
-#TODO: Allow to edit the definition
+# TODO: Permitir editar la definición
 
-targetFile='/path/to/file/myFile.txt'
+targetFile = '/home/david/Downloads/Anki Decks/ankiVocDeck.txt'
+
 
 def pref(x):
     return {
@@ -13,58 +14,68 @@ def pref(x):
         "adjetivo\n": "(adj.) ",
         "adverbio\n": "(adv.) ",
         "preposición\n": "(prep.) ",
-		"exclamación\n": "(excl.)"
-    }.get(x, "");   # "" is default if x not found
+        "exclamación\n": "(excl.)",
+        "conjunción\n": "(conj.)"
+    }.get(x, "")   # "" is default if x not found
 
 
 os.system("xclip -o > .text")
-f = open ('.text', 'r') 
+f = open('.text', 'r')
 concept = f.read().split()
 if len(concept) == 1:
     definitions = ""
     os.system("trans -d en:en `cat .text` > .translation")
 
     f = open('.translation', 'r')
-    concept = f.readline() 
+    concept = f.readline()
     concept = concept[:-1]
+    concept = concept.lower()
     phonetic = f.readline()
-    if phonetic[0] == '/': #There was a phonetic line
-        f.readline() # Now there is an empty line then
+    if phonetic[0] == '/':  # There was a phonetic line
+        f.readline()  # Now there is an empty line then
     line = f.readline()
-    prefix = pref (line)
+    prefix = pref(line)
     finish = prefix == ""
     line = f.readline().split()
     counter = 0
-    while finish != True:
+    while finish is not True:
         counter += 1
         line = line[1:len(line)]
-        line[len(line)-1] = line[len(line)-1].split('.')[0]
+        if len(line) == 0:
+            break
+        line[len(line) - 1] = line[len(line) - 1].split('.')[0]
         definitions += "FALSE \"\\\""
         definitions += prefix
         for word in line:
             definitions += word + " "
-        definitions += "(e.g. "
-        line = f.readline() # read example
-        line = line[line.find("\"")+1:]
-       
-        #remove concept variations in the examples
-        line = line[: -1].replace(concept + "s", ".....")
-        line = line.replace(concept + "es", ".....")
-        line = line.replace(concept + "ed", ".....")
-        line = line.replace(concept + concept[-1:] + "ed", ".....")
-        line = line.replace(concept + "ing", ".....")
-        line = line.replace(concept + concept[-1:] + "ing", ".....")
-        if concept[-1:] == 'y':
-            line = line[: -1].replace(concept[:-1]+ "ies", ".....")
-        line = line[: -1].replace(concept, ".....")
+        line = f.readline()  # read example
+        if line[0] == '"':
+            definitions += "(e.g. "
+            line = line[line.find("\"") + 1:]
 
-        definitions += line
-        definitions += ")\\\"; " + concept + "\" "
+            # remove concept variations
+            dots = "....."
+            line = line[: -1].replace(concept + "s", dots)
+            line = line.replace(concept + "es", dots)
+            if concept[-1] == 'e':
+                line = line.replace(concept + "d", dots)
+            line = line.replace(concept + "ed", dots)
+            line = line.replace(concept + concept[-1:] + "ed", dots)
+            line = line.replace(concept + "ing", dots)
+            line = line.replace(concept + concept[-1:] + "ing", dots)
+            if concept[-1:] == 'y':
+                line = line[: -1].replace(concept[:-1] + "ies", dots)
+            line = line[: -1].replace(concept, dots)
+
+            definitions += line + ")"
+        definitions += "\\\"; " + concept + "\" "
         line = f.readline()
+        if line == "Sinónimos\n" or line == "Ejemplos\n" or line == "Ver también":
+            finish = True
         while line != "\n":
             line = f.readline()
-        line =  f.readline()
-        if line == "Sinónimos\n" or line == "Ejemplos\n":
+        line = f.readline()
+        if line == "Sinónimos\n" or line == "Ejemplos\n" or line == "Ver también":
             finish = True
         else:
             new_prefix = pref(line)
@@ -75,15 +86,17 @@ if len(concept) == 1:
                 line = line.split()
     if counter == 1:
         definitions = definitions.replace("\\\"", "\"")[7:-2]
-        confirm = os.system( "zenity --question --ok-label=\"Ok\" --cancel-label=\"Cancelar\"  --height=10 --text=\"" + "Añadida a anki la tarjeta:\n" + definitions.replace("\"", "\\\"") + "\"")
+        confirm = os.system("zenity --question --ok-label=\"Ok\" --cancel-label=\"Cancelar\"  --height=10 --text=\"" +
+                            "Añadida a anki la tarjeta:\n" + definitions.replace("\"", "\\\"") + "\"")
         if confirm == 0:
             f = open(targetFile, 'a')
             f.write(definitions + "\n")
-            f.close()        
+            f.close()
     elif counter > 1:
-        os.system("ans=$(zenity  --list --text '$word - Seleciona las definiciones para añadir a la base de datos de Anki' --checklist  --column \"Pick\" --column \"Definitions\" " + definitions + "  --width=500 --height=450); echo $ans > .answer")
+        os.system("ans=$(zenity  --list --text '" + concept + " - Seleciona las definiciones para añadir a la base de datos de Anki' --checklist  --column \"Pick\" --column \"Definitions\" " +
+                  definitions + "  --width=500 --height=450); echo $ans > .answer")
         f.close()
-        
+
         f = open('.answer', 'r')
         lines = f.read()
         lines = lines[:-1].split('|')
@@ -94,9 +107,7 @@ if len(concept) == 1:
             f.write(line + "\n")
         f.close()
         os.system("rm .answer")
-    else: #no definitions found
-        os.system( "zenity --question --ok-label=\"Ok\" --cancel-label=\"Cancelar\"  --height=10 --text=\"No se ha encontrado una definición para \\\"" + concept + "\\\" \""  )
+    else:  # no definitions found
+        os.system("zenity --question --ok-label=\"Ok\" --cancel-label=\"Cancelar\"  --height=10 --text=\"No se ha encontrado una definición para \\\"" + concept + "\\\" \"")
 
     os.system("rm .text .translation")
-
-
